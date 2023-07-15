f2dc987238ffb1f0a0930803b21cf0e3dd5857cb
Md Shaquib Alam <shaquib475@gmail.com>
2023-07-15 15:42:01 +0530

    userModel

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:[true , 'username is required']
+    },
+    email:{
+        type:String,
+        required:[true , 'email is required']
+    },
+    password:{
+        type:String, 
+        required:[true , 'password is requied']
+    }
+} , {timestamps:true})
+
+
+const userModel = mongoose.model('User' , userSchema);
+
+module.exports = userModel;
+
+
