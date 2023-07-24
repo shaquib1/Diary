783b77e5f974d42acaeb98f63c6ab975049c5ce8
Md Shaquib Alam <shaquib475@gmail.com>
2023-07-25 02:07:05 +0530

    redux store

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,13 @@
 import  React ,{useState} from 'react';
 import {Box , AppBar , Toolbar , Button , Typography, Tabs,Tab} from "@mui/material"
 import { Link } from 'react-router-dom';
-
+import {useSelector} from "react-redux"
 
 const Header = () => {
-
+   //global state
+   const isLogin = useSelector(state => state.isLogin)
+      console.log(isLogin);
+    //state
    const [value , setValue] = useState();
 
   return (
@@ -12,16 +15,25 @@ const Header = () => {
     <AppBar position='sticky'>
      <Toolbar>
         <Typography variant='h4'>My Blog App</Typography>
-        <Box display={"flex"} margin="auto" marginRight={"auto"}>
-           <Tabs textColor='inherit' value={value} onChange={(e,val)=> setValue(val)}>
-            <Tab label="Blogs" LinkComponent={Link} to="blogs" />
-            <Tab label="My Blogs" LinkComponent={Link} to="my-blogs"/>
-           </Tabs>
-        </Box>
+        {isLogin && (
+         <Box display={"flex"} margin="auto" marginRight={"auto"}>
+         <Tabs textColor='inherit' value={value} onChange={(e,val)=> setValue(val)}>
+          <Tab label="Blogs" LinkComponent={Link} to="blogs" />
+          <Tab label="My Blogs" LinkComponent={Link} to="my-blogs"/>
+         </Tabs>
+      </Box>
+        )}
         <Box display={"flex"} marginLeft="auto">
-           <Button sx={{margin:1 , color:'white'}} LinkComponent={Link} to="/login"> Login</Button>
+          {!isLogin && (
+            <>
+             <Button sx={{margin:1 , color:'white'}} LinkComponent={Link} to="/login"> Login</Button>
            <Button sx={{margin:1 , color:'white'}} LinkComponent={Link} to="/register"> Regiter</Button>
-           <Button sx={{margin:1 , color:'white'}}> Logout</Button>
+           </>
+          )}
+           
+           {isLogin &&(
+            <Button sx={{margin:1 , color:'white'}}> Logout</Button>
+           )}
         </Box>
 
      </Toolbar>
