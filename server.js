2f54c2bb13e8487ead8ea83f337e0337a1cf31e8
Md Shaquib Alam <shaquib475@gmail.com>
2023-07-27 22:48:29 +0530

    server

--- server.js
+++ server.js
@@ -1,47 +1,40 @@
-const express =require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const colors = require('colors')
-const dotenv =require('dotenv')
-const conncetDB  = require('./config/db')
-
-
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const conncetDB = require("./config/db");
 
 // env config
 dotenv.config();
 
+//router import
+const userRoutes = require("./routes/userRoutes");
+const blogRoutes = require("./routes/blogRoutes");
 
-//router import 
-const userRoutes = require('./routes/userRoutes')
-const blogRoutes = require('./routes/blogRoutes')
-
-// mongodb connection 
+// mongodb connection
 conncetDB();
 
-
-
 //rest object
-const app=express();
-
+const app = express();
 
 //middlewares
-app.use(cors())
+app.use(cors());
 
-app.use(express.json({ limit: '1mb' }));
-app.use(express.urlencoded({ limit: '1mb', extended: true }));
-app.use(morgan('dev'))
-
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ limit: "1mb", extended: true }));
+app.use(morgan("dev"));
 
 // routes
-app.use('/api/v1/user' , userRoutes);
-app.use('/api/v1/blog' , blogRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/blog", blogRoutes);
 
+// port
+const PORT = process.env.PORT || 8080;
 
-// port 
-const PORT= process.env.PORT  || 8080
-
-
-// listen 
-app.listen(PORT , ()=>{
-    console.log(` server is running on ${process.env.DEV_MODE} on port ${PORT}`.bgCyan.black);
+// listen
+app.listen(PORT, () => {
+  console.log(
+    ` server is running on ${process.env.DEV_MODE} on port ${PORT}`.bgCyan.black
+  );
 });
