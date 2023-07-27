77ba2ed7e6e28f37522166ee2dd892f60d11cc8d
Md Shaquib Alam <shaquib475@gmail.com>
2023-07-27 22:49:21 +0530

    database connection

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // for altas connection try it
 //mongoose.set("strictQuery" , true);
 
-
-const conncetDB = async()=>{
-
-    try {
-        const conn = await mongoose.connect(process.env.Mongo_URL);
-        console.log(`app is connected to mongodb database ${conn.connection.host }`.bgMagenta)
-    } catch (error) {
-        console.log(`mongodb connect to Error ${error}`.bgBlue.white)
-    }
-}
+const conncetDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.Mongo_URL);
+    console.log(
+      `app is connected to mongodb database ${conn.connection.host}`.bgMagenta
+    );
+  } catch (error) {
+    console.log(`mongodb connect to Error ${error}`.bgBlue.white);
+  }
+};
 
 //for connecting databse from mongodb altas
 //mongodb+srv://shaquib:mgpass@cluster0.ai9ki2k.mongodb.net/dbname=blogApp
