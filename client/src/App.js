d1236b93a147ca99ba9eab3815f4015c5a48b5a6
Md Shaquib Alam <shaquib475@gmail.com>
2023-07-27 22:59:30 +0530

    checking code format

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
 import "./App.css";
 import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom";
-import Blogs from './pages/Blogs';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Blogs from "./pages/Blogs";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Blogs />} />
-      <Route path="/blogs" element={<Blogs/>} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Blogs />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </>
   );
-  
 }
 
 export default App;
